cd8c5cb605347c304e37dca022206e8c7f2b8ebe
sergiolabaguette <sergiolabaguette@gmail.com>
2022-02-15 08:33:17 +0100

    refactor

--- dm542.c
+++ dm542.c
@@ -39,19 +39,20 @@ static void set_direction(int direction){
     /* 1 for UP, 0 for DOWN */
     enum Setup gpio_pin_dir = DIR;
     gpio_put(gpio_pin_dir, direction);
-    busy_wait_ms(500);
+    /* DIR signal must be ahead of PUL by 500 us to ensure correct direction */
+    busy_wait_us(500);
 }
 
-static void run (int gpio_pin_ena, int gpio_pin_pul, int step_delay, int steps_to_move){
+static void full_rotation (int gpio_pin_ena, int gpio_pin_pul, int step_delay, int steps_to_move){
     /* false enables driver, true disables driver */
     gpio_put(gpio_pin_ena, 0);
-    /* DIR signal must be ahead of PUL by 500 us to ensure correct direction */
     for (size_t i = 0; i < steps_to_move; ++i){
         step_once(gpio_pin_pul, step_delay);
     };
     /* turn off the driver after every run to reduce heat */
     gpio_put(gpio_pin_ena, 1);
 }
+
 int main() {
     enum Setup step_delay = DELAY;
     enum Setup step_count = STEPS;
@@ -59,19 +60,16 @@ int main() {
     enum Setup gpio_pin_dir = DIR;
     enum Setup gpio_pin_ena = ENA;
 
+    /* always run setup_pins before anything else */
     setup_pins(gpio_pin_pul, gpio_pin_dir, gpio_pin_ena);
 
-    set_direction(1);
-    run(gpio_pin_ena, gpio_pin_pul, step_delay, 24000);
-
-
+    /* say hello to my little friend */
     for (size_t j = 0; j < 5; ++j){
         set_direction(0);
-        run(gpio_pin_ena, gpio_pin_pul, step_delay, step_count);
+        full_rotation(gpio_pin_ena, gpio_pin_pul, step_delay, step_count);
         set_direction(1);
-        run(gpio_pin_ena, gpio_pin_pul, step_delay, step_count);
+        full_rotation(gpio_pin_ena, gpio_pin_pul, step_delay, step_count);
     }
-
 
     return 0;
 }
